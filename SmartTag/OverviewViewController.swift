43693e5da14a05edeffa50b2cedf8d01ad71c8ab
Jackie Yang <me@jackieyang.me>
2017-05-17 23:40:52 -0700

    Create basic interfaces

---
+++ SmartTag/OverviewViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MainViewController.swift
+//  SmartTag
+//
+//  Created by Jackie Yang on 5/17/17.
+//  Copyright © 2017 Jackie Yang. All rights reserved.
+//
+import UIKit
+
+class OverviewViewController: UIViewController {
+    
+    let presentSigninViewSegueIdentifier = "presentSigninView"
+    
+    var signedIn = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if signedIn == false {
+            self.performSegue(withIdentifier: presentSigninViewSegueIdentifier, sender: self)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == presentSigninViewSegueIdentifier {
+            (segue.destination as! SigninViewController).parentView = self
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func initializeData() -> Void {
+        signedIn = true;
+    }
+}
+
--- SmartTag/SigninViewController.swift
+++ SmartTag/SigninViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SigninViewController: UITableViewController {
+    
+    var parentView: OverviewViewController?;
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func signIn(_ sender: UIButton) {
+        self.dismiss(animated: true) {
+            self.parentView?.initializeData()
+        }
+    }
 }
 
